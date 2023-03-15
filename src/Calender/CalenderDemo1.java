41108e0cb0a5a0ea5b2cc3c23c502fa7c16923fb
Adnan <adnanmohammad7@gmail.com>
2023-03-15 21:40:16 +0530

    This is commit from Adnan

--- src/Calender/CalenderDemo1.java
+++ src/Calender/CalenderDemo1.java
@@ -60,6 +60,8 @@ public class CalenderDemo1 {
 		}
 		driver.findElement(By.xpath("//div[@data-testid='undefined-month-August-2023']/div[3]/div[4]/div[6]")).click();
 		
+		System.out.println("Data is selected successfully");
+		
 		
 		
 		
